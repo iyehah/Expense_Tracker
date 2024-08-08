ea109388d97dc7aedee0ef49622763d3b6702ef4
Iyehah <117352746+iyehah@users.noreply.github.com>
2024-08-08 12:17:44 +0000

    Fix: Fix the error

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ExpenseItem = ({ expense }) => {
   return (
     <li>
-      <strong>{expense}</strong> {expense.amount} UMR
+      <strong>{expense.name}</strong> {expense.amount} UMR
     </li>
   );
 };
